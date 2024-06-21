2ed3ff5a0653ff66fd5df4e1d3ff62e71f89bb54
Antoni Dawiowicz <s27366@pjwstk.edu.pl>
2024-06-21 11:12:49 +0200

    f

---
+++ sevicePopraw/Services/IStudentsService.cs
@@ -0,0 +1,9 @@
+﻿using sevicePopraw.models;
+
+namespace sevicePopraw.Services;
+
+public interface IStudentsService
+{
+    IEnumerable<Student> GetStudents();
+    int CreateStudent(Student student);
+}
---
+++ sevicePopraw/Services/StudentsService.cs
@@ -0,0 +1,34 @@
+﻿using sevicePopraw.models;
+
+namespace sevicePopraw.Services;
+
+public class StudentsService : IStudentsService
+{
+    private static readonly List<Student> _students = new()
+    {
+        new Student
+        {
+            IdStudent = 1, FirstName = "jan", LastName = "ss", Address = "waw", Email = "asdsd", IndexNumber = 1
+        },
+        new Student
+        {
+            IdStudent = 2, FirstName = "hg", LastName = "uiui", Address = "fghgfh", Email = "xxxxx", IndexNumber = 2
+        },
+        new Student
+        {
+            IdStudent = 3, FirstName = "xcg", LastName = "nnns", Address = "gfhfgh", Email = "vmv", IndexNumber = 3
+        }
+    };
+    
+    
+    public IEnumerable<Student> GetStudents()
+    {
+        return _students;
+    }
+
+    public int CreateStudent(Student student)
+    {
+        _students.Add(student);
+        return 1;
+    }
+}
